d61a1a53d547c8da0cd6499b7e36048c3bf52ac8
Bo Cupp <bobocupp@gmail.com>
2026-05-04 23:35:37 -0700

    Lesson3 starting point work in progress

--- RoboRace.ino
+++ RoboRace.ino
@@ -11,6 +11,118 @@ namespace Pins {
   constexpr int Button = 2;
 }
 
+class StepperMotor {
+  public:
+    StepperMotor(int stepPin, int dirPin) :
+      stepPin_(stepPin),
+      dirPin_(dirPin)
+    {}
+
+    void step(bool direction) {
+      // Change direction if needed
+      if (lastDirection_ != direction) {
+        lastDirection_ = direction;
+        if (direction) {
+          digitalWrite(dirPin_, HIGH);
+        } else {
+          digitalWrite(dirPin_, LOW);
+        }
+      }
+
+      digitalWrite(stepPin_, HIGH);
+      delayMicroseconds(2);
+      digitalWrite(stepPin_, LOW);
+    }
+
+  private:
+    bool lastDirection_ = true;
+    int stepPin_;
+    int dirPin_;
+};
+
+class StepperMotionController {
+public:
+  StepperMotionController(StepperMotor& stepper) :
+    stepper_(stepper)
+  {}
+
+  void setTarget(int steps, int maxStepsPerS, int maxStepsPerSPerS) {
+    targetSteps_ = steps;
+    remainingSteps_ = steps;
+    maxStepsPerS_ = maxStepsPerS;
+    maxStepsPerSPerS_ = maxStepsPerSPerS;
+
+    // Calculate nextStepDueS
+    nextStepDueS_ = 0.0;
+  }
+
+  bool update() {
+  }
+private:
+  double nextStepDueS_ = 0.0;
+  int maxStepsPerS_ = 0;
+  int maxStepsPerSPerS_ = 0;
+  int remainingSteps_ = 0;
+  int targetSteps_ = 0;
+  StepperMotor& stepper_;
+};
+
+class Robot {
+public:
+  void forward(double distanceM) {
+  }
+
+  void reverse(double distanceM) {
+  }
+
+  void left() {
+  }
+
+  void right() {
+  }
+
+private:
+  StepperMotionController m1Motion_;
+  StepperMotionController m2Motion_;
+  StepperMotor m1_;
+  StepperMotor m2_;
+};
+
+enum class RobotMoveType {
+  forward,
+  backward,
+  left,
+  right
+};
+
+struct RobotMove {
+  RobotMoveType moveType;
+  double distanceM;
+};
+
+class RobotControl {
+public:
+  // Runs all moves atomically (doesn't return until moves are completed)
+  static void runMovesOnRobot(RobotMove moves[], Robot& robot) {
+    for(auto move : moves) {
+      switch(move.moveType) {
+        case RobotMoveType::forward:
+          robot.foward(move.distanceM);
+          break;
+        case RobotMoveType::backward:
+          robot.backward(move.distanceM);
+          break;
+        case RobotMoveType::left:
+          robot.left();
+          break;
+        case RobotMoveType::right:
+          robot.right(move.distanceM);
+          break;
+      }
+    }
+  }
+};
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(Pins::M1::Step, OUTPUT);
@@ -75,15 +187,20 @@ void step() {
 }
 
 void loop() {
+  static std::array<RobotMove> moves = {
+    {RobotMoveType::forward, 0.5},
+    {RobotMoveType::backward, 0.5},
+    {RobotMoveType::left, 0.0},
+    {RobotMoveType::right, 0.0},
+    {RobotMoveType::forward, 2.0},
+    {RobotMoveType::backward, 2.0},
+    {RobotMoveType::left, 0.0},
+    {RobotMoveType::right, 0.0}
+  };
+
   // Wait here until we press the button
   while (!buttonPressed()) {}
 
-  long lastStepTaken = 0;
-  while (!buttonPressed()) {
-    long currentTime = millis();
-    if (currentTime - lastStepTaken > 500) {
-      step();
-      lastStepTaken = currentTime;
-    }
-  }
+  Robot robot;
+  RobotControl::runMovesOnRobot(moves, robot);
 }
